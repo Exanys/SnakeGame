9f23b883af13f25f57b872a5121977959168f486
Exanys <exanys226@gmail.com>
2020-12-19 14:09:04 +0100

    v1.4

--- SnakeGame.js
+++ SnakeGame.js
@@ -3,57 +3,54 @@ const ctx = canvas.getContext("2d");
 //platno
 ctx.fillStyle = '#4c4c4c';
 ctx.fillRect(0, 0, canvas.width, canvas.height);
+document.getElementById("NewGame").addEventListener("click", () => {
+  game.play();
+}); 
 
 let game = {
-    x: 0, 
-    y: 0,
-    scale: 25,
-    columns: 0,
-    rows: 0,
-    timer: null,
-    speed: 250,
+  x: 0,
+  y: 0,
+  scale: 25,
+  columns: 0,
+  rows: 0,
+  timer: null,
+  speed: 250,
 
-     play: function() {
-        this.columns = canvas.width / this.scale;
-        this.rows = canvas.height / this.scale;
+  play: function () {
+    this.columns = canvas.width / this.scale;
+    this.rows = canvas.height / this.scale;
 
-        let snake = new Snake();
-        let food = new Food();
+    let snake = new Snake();
+    let food = new Food();
 
-        document.addEventListener('keydown', function(event){
-            snake.direction(event.code);
-        });
+    document.addEventListener("keydown", function (event) {
+      snake.direction(event.code);
+    });
 
+    food.position();
+
+    this.timer = setInterval(() => {
+      ctx.fillStyle = "#4c4c4c";
+      ctx.fillRect(0, 0, canvas.width, canvas.height);
+      food.draw();
+      snake.position();
+      if(snake.bite()){
+        this.gameOver();
+        console.log("over");
+    }
+      snake.move();
+      if (snake.eating(food.x, food.y)) {
+        snake.count++;
         food.position();
+      }
 
-        this.timer = setInterval(() => {
-            ctx.fillStyle = '#4c4c4c';
-            ctx.fillRect(0, 0, canvas.width, canvas.height);
-            food.draw();
-            snake.position();
-            snake.move();
-            if(snake.eating(food.x, food.y)){
-                snake.count++;
-                snake.tailPos.push({x: food.x, y: food.y});
-                food.position();
-
-            }
-            // if(snake.bite()){
-            //     this.gameOver();
-            //     console.log("over");
-            // }
-
-        }, this.speed);
-
-
-    }//, 
-    // gameOver: () => {
-    //     clearInterval(this.timer);
-    //     this.time = null;
-    // }
+    }, this.speed);
+  }, 
+  gameOver: function(){
+    clearInterval(this.timer);
+    this.timer = null;
+  }
 }
-
-game.play();
 
 function Snake(){
     this.x = 0,
@@ -125,14 +122,14 @@ function Snake(){
         if(this.x == x && this.y == y) {
             return true;
         }
-    }//,
-    /*this.bite = function(){
-        this.tailPos.forEach((value) =>{
-            if(value.x == this.x && value.y == this.y){
+    },
+    this.bite = function(){
+        for (let i = 0; i < this.tailPos.length -1; i++) {
+            if (this.tailPos[i].x == this.x && this.tailPos[i].y == this.y){
                 return true;
-            }
-        });
-    }*/
+          }
+        }
+    }
 }
 
 function Food() {
